4c2b0abd694032a7d309e316669a89ae34880497
Bruno St-Denis <77029725+BSt-Denis@users.noreply.github.com>
2022-02-24 20:33:11 -0500

    Change name base selection to month numeric valur instead

--- R/bymonth_var.R
+++ R/bymonth_var.R
@@ -1,29 +1,30 @@
-#' @title Extract variable list data  according to specified months
+#' @title Extract data from a variable list according to specified months
 #'
 #' @description Extract data from a var_list by specifying which month the data
 #' should contain.
 #'
 #' @param var_list var_list created or loaded by this package
-#' @param month_name Name of the months to use for extraction. Either character
-#' or vector of character
+#' @param month_name Numeric number of the month (11 = november). Either numeric
+#' or vector numeric element
 #'
-#' @return var_list containing data for the specified month
+#' @return var_list containing data of the specified month
 #'
 #' @export
 #'
-#' @examples \dontrun{temp_june <- bymonth_var(total_temp,"june")
-#' temp_june_august <- bymonth_var(total_temp,c("june","august"))}
+#' @examples \dontrun{temp_june <- bymonth_var(total_temp,6)
+#' temp_june_august <- bymonth_var(total_temp,c(06,08))}
 #'
 bymonth_var <- function(var_list, month_name){
+
+  #Check if time is in the var_list
   if(!("time" %in% names(var_list))){
     stop("Element time must be part of the var_list argument")
   }
-
-  # Lower possible capital letter
-  month_name = tolower(month_name)
+  # Check if month_name is numeric
+  stopifnot(is.numeric(month_name))
 
   # Extract month from date time
-  monthName = tolower(lubridate::month(var_list$time, label=TRUE, abbr=FALSE))
+  monthName = lubridate::month(var_list$time)
 
   # Get valid date index
   valid_date_id = which(monthName %in% month_name)
