026d52185963276ee223e317394a8348ef3afb7e
Bruno St-Denis <77029725+BSt-Denis@users.noreply.github.com>
2022-03-23 23:57:44 -0400

    Create internal function inside the package

---
+++ R/toolbox.R
@@ -0,0 +1,28 @@
+slice_xy <- function(vl, longitude, latitude){
+  # Check for longitude and latitude coodinates, and dimensions of data
+  stopifnot("longitude" %in% names(var_list),"latitude" %in% names(var_list))
+
+  # Get index of valid coordinates
+  valid_lon_id = which(dplyr::between(vl$longitude[,1], min(longitude), max(longitude)))
+  valid_lat_id = which(dplyr::between(vl$latitude[1,], min(latitude), max(latitude)))
+
+  # Slicing coordinates
+  vl[["longitude"]] = vl$longitude[valid_lon_id,valid_lat_id]
+  vl[["latitude"]] = vl$latitude[valid_lon_id,valid_lat_id]
+
+  # Slicing the variables
+  if(length(vl$shape) == 2){
+    for(vars in vl$variables){
+      vl[[vars]] = vl[[vars]][valid_lon_id,valid_lat_id]
+    }
+  } else {
+    for(vars in vl$variables){
+      vl[[vars]] = vl[[vars]][valid_lon_id,valid_lat_id,]
+    }
+  }
+
+  # Updating var_list
+  vl[["shape"]] = dim(vl[[vars]])
+
+  return(vl)
+}
