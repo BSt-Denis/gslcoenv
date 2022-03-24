f04f307b3d92ce6f6015cfe855d652c818a9b0a2
Bruno St-Denis <77029725+BSt-Denis@users.noreply.github.com>
2022-03-24 13:18:18 -0400

    Clarify code and manual

--- R/bymonth_var.R
+++ R/bymonth_var.R
@@ -4,8 +4,8 @@
 #' should contain.
 #'
 #' @param var_list var_list created or loaded by this package
-#' @param month_name Numeric number of the month (11 = november). Either numeric
-#' or vector numeric element
+#' @param month_num Numeric number of the month (11 = november). Either numeric
+#' or vector of numeric elements
 #'
 #' @return var_list containing data of the specified month
 #'
@@ -14,20 +14,16 @@
 #' @examples \dontrun{temp_june <- bymonth_var(total_temp,6)
 #' temp_june_august <- bymonth_var(total_temp,c(06,08))}
 #'
-bymonth_var <- function(var_list, month_name){
+bymonth_var <- function(var_list, month_num){
 
   #Check if time is in the var_list
-  if(!("time" %in% names(var_list))){
-    stop("Element time must be part of the var_list argument")
-  }
-  # Check if month_name is numeric
-  stopifnot(is.numeric(month_name))
+  stopifnot("time" %in% names(var_list), is.numeric(month_num))
 
   # Extract month from date time
-  monthName = lubridate::month(var_list$time)
+  monthNum = lubridate::month(var_list$time)
 
   # Get valid date index
-  valid_date_id = which(monthName %in% month_name)
+  valid_date_id = which(monthNum %in% month_num)
 
   # Get valid time values
   var_list$time = var_list$time[valid_date_id]
